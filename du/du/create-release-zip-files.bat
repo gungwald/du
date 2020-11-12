f984583cc1cf4917ad7673f8a33e584e660aa2e4
Bill Chatfield <bill_chatfield@yahoo.com>
2020-11-12 02:21:52 -0500

    Created script to bundle release zips

---
+++ du/du/create-release-zip-files.bat
@@ -0,0 +1,5 @@
+@echo off
+powershell Compress-Archive -LiteralPath '..\Release\du.exe'	   -DestinationPath "Disk_Usage_for_Windows_x86_32-bit.zip"
+powershell Compress-Archive -LiteralPath '..\x64\Release\du.exe'   -DestinationPath "Disk_Usage_for_Windows_x64.zip"
+powershell Compress-Archive -LiteralPath '..\ARM\Release\du.exe'   -DestinationPath "Disk_Usage_for_Windows_ARM32.zip"
+powershell Compress-Archive -LiteralPath '..\ARM64\Release\du.exe' -DestinationPath "Disk_Usage_for_Windows_ARM64.zip"
